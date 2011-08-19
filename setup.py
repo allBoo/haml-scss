d7bc1ab0a95ce8dbacfe4947f509c574281c5471
Dan LaMotte <lamotte85@gmail.com>
2011-08-18 20:35:52 -0500

    setup: require pyscss

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.markdown')).read(),
     url='https://bitbucket.org/dlamotte/haml-scss',
     keywords='haml sass scss django template',
+    install_requires=[
+        'pyScss>=1.0.8',
+    ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False

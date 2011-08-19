9a059561da9a84ae7e72f0192b92d2ad7c0d041f
Dan LaMotte <lamotte85@gmail.com>
2011-08-18 20:27:45 -0500

    setup: adding some flags

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 setup(
-    name='haml-scss',
-    version='0.5.1',
-    packages=['haml', 'scss_integ', ],
     author='Dan LaMotte',
     author_email='lamotte85@gmail.com',
+    name='haml-scss',
+    version='0.5.1',
     description='haml for python (scss coming soon)',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.markdown')).read(),
-    keywords='haml sass scss django template',
     url='https://bitbucket.org/dlamotte/haml-scss',
+    keywords='haml sass scss django template',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
 )

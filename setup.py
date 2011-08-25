e2c343ebebbdf30d55b660c182d46b69d30c6cac
Dan LaMotte <lamotte85@gmail.com>
2011-08-24 22:04:34 -0500

    new version 0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Dan LaMotte',
     author_email='lamotte85@gmail.com',
     name='haml-scss',
-    version='0.6.2',
+    version='0.7',
     description='haml and scss for python',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.markdown')).read(),
     url='https://bitbucket.org/dlamotte/haml-scss',

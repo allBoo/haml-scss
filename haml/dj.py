594512133026e0b5724be2ae3e828be5d891186e
Alex Kazinskiy <alboo@list.ru>
2019-06-04 16:05:21 +0400

    support for Django 1.11

--- haml/dj.py
+++ haml/dj.py
@@ -12,35 +12,17 @@ except ImportError:
 class FSLoader(DjFSLoader):
     is_usable = True
 
-    def load_template_source(self, template_name, template_dirs=None):
-        data, fn = super(FSLoader, self) \
-            .load_template_source(template_name, template_dirs)
-
-        if not template_name.endswith('.haml'):
-            return data, fn
+    def get_contents(self, origin):
+        if not origin.name.endswith('.haml'):
+            return super(FSLoader, self).get_contents(origin)
 
         out = StringIO()
         # TODO nasty open(fn) workaround .decode(settings.FILE_CHARSET) issue.
         # regex in compiler module stops matching... unsure why
-        Compiler(open(fn), out).compile()
+        Compiler(open(origin.name), out).compile()
+        return out.getvalue()
+    get_contents.is_usable = True
 
-        return out.getvalue(), fn
-    load_template_source.is_usable = True
 
-class AppLoader(DjAppLoader):
-    is_usable = True
-
-    def load_template_source(self, template_name, template_dirs=None):
-        data, fn = super(AppLoader, self) \
-            .load_template_source(template_name, template_dirs)
-
-        if not template_name.endswith('.haml'):
-            return data, fn
-
-        out = StringIO()
-        # TODO nasty open(fn) workaround .decode(settings.FILE_CHARSET) issue.
-        # regex in compiler module stops matching... unsure why
-        Compiler(open(fn), out).compile()
-
-        return out.getvalue(), fn
-    load_template_source.is_usable = True
+class AppLoader(FSLoader, DjAppLoader):
+    pass
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     author='Dan LaMotte',
     author_email='lamotte85@gmail.com',
     name='haml-scss',
-    version='0.7.3',
+    version='0.8.0',
     description='haml and scss for python',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.markdown')).read(),
-    url='https://bitbucket.org/dlamotte/haml-scss',
+    url='git@github.com:allBoo/haml-scss.git',
     keywords='haml sass scss django integration template',
     install_requires=[
         'pyScss>=1.0.8',
